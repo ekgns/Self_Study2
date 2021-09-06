095cadb43e83d581ff87bd2384176d5e5d45787d
kimdahun.hello <kimdahun.hello@gmail.com>
2021-09-06 22:28:32 +0900

    명언생성기 완성

---
+++ WiseSayCenerator_fcamPractice/WiseSayCenerator_fcamPractice/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  WiseSayCenerator_fcamPractice
+//
+//  Created by 다훈김 on 2021/09/06.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
