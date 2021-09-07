782ae0ca503a9f83187f17e109bcc54658166a3d
kimdahun.hello <kimdahun.hello@gmail.com>
2021-09-07 22:32:02 +0900

    SegueViewController add

---
+++ LEDSignAPP_Practice/LEDSignAPP_Practice/SeguePushViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SeguePushViewController.swift
+//  LEDSignAPP_Practice
+//
+//  Created by 다훈김 on 2021/09/07.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
