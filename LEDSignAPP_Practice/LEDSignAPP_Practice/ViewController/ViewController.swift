776fa5dccd373e735fa12a8f41c07c2283ab9e5b
kimdahun.hello <kimdahun.hello@gmail.com>
2021-09-07 23:10:36 +0900

    CodePresent 완성 style FullScreen으로 변경

--- LEDSignAPP_Practice/LEDSignAPP_Practice/ViewController/ViewController.swift
+++ LEDSignAPP_Practice/LEDSignAPP_Practice/ViewController/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    @IBAction func tapCodePresentButton(_ sender: UIButton) {
+        guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") else { return }
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
     @IBAction func tapCodePushButton(_ sender: UIButton) {
         guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePushViewController") else { return }
         self.navigationController?.pushViewController(viewController, animated: true)
