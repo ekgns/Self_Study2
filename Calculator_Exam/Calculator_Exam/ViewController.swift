5d403252b5f74a0a558cb3ecfefb1dc9526837f7
ekgns <ekgns14@naver.com>
2021-10-18 10:20:09 +0900

    계산기 만들기

---
+++ Calculator_Exam/Calculator_Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator_Exam
+//
+//  Created by 다훈김 on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++
@@ -1,8 +0,0 @@
-# Self_Study2
-
-## 패캠강의 
-## 기초복습 
-WiseSayCenerator_fcamPractice 
-명언 생성기 (폴더안 리드미 작성)
-LEDSignApp LED 전광판 
-1. 화면 전환 방법에 대한 복습과정
